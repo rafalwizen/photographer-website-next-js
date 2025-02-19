98b21c4a4f6ad32ff948ef3e97753062e50daffa
Rafal Wizen <rafal.wizen@gmail.com>
2025-02-19 20:03:59 +0100

    Update images

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,7 @@
-import {useTranslations} from 'next-intl';
-import {Link} from '@/i18n/routing';
+import About from "@/components/about/About";
 
 export default function HomePage() {
-    const t = useTranslations('navbar');
     return (
-        <div>
-            <h1>{t('home')}</h1>
-            <Link href="/about">{t('about_me')}</Link>
-        </div>
+        <About />
     );
 }
